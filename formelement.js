0a057a590df95b87fc72a0e7e98b6e1efdfc43d7
KoryNunn <knunn187@gmail.com>
2014-07-14 16:56:39 +1000

    improved validation, correct enabled setting

--- formelement.js
+++ formelement.js
@@ -2,25 +2,20 @@ var Gaffa = require('gaffa'),
     crel = require('crel'),
     doc = require('doc-js');
 
-function setValue(event){
-    var input = event.target,
-        view = input.viewModel;
-
-    view.value.set(input.value);
-}
-
 function FormElement(){}
 FormElement = Gaffa.createSpec(FormElement, Gaffa.View);
 FormElement.prototype._type = 'formElement';
 
 FormElement.prototype.render = function(){
-    var renderedElement = this.renderedElement = this.renderedElement || crel('input'),
+    var view = this,
+        renderedElement = this.renderedElement = this.renderedElement || crel('input'),
         formElement = this.formElement = this.formElement || renderedElement,
         updateEventNames = (this.updateEventName || "change").split(' ');
 
-    this._watch(formElement);
-
-    this.gaffa.events.on(this.updateEventName || "change", formElement, setValue);
+    doc.on(this.updateEventName || "change", formElement, function(){
+        view.value.set(formElement.value);
+        view.valid.set(formElement.validity.valid);
+    });
 };
 
 FormElement.prototype.value = new Gaffa.Property(function(view, value){
@@ -62,6 +57,8 @@ FormElement.prototype.value = new Gaffa.Property(function(view, value){
             element.setSelectionRange(caretPosition, caretPosition);
         }
     }
+
+    view.valid.set(element.validity.valid);
 });
 
 FormElement.prototype.type = new Gaffa.Property(function(view, value){
@@ -80,10 +77,17 @@ FormElement.prototype.required = new Gaffa.Property(function(view, value){
     }
 });
 
+FormElement.prototype.valid = new Gaffa.Property();
 FormElement.prototype.validity = new Gaffa.Property(function(view, value){
     value = value || '';
 
     view.formElement.setCustomValidity(value);
+});
+FormElement.prototype.enabled = new Gaffa.Property({
+    update: function(view, value){
+        view.formElement[value ? 'removeAttribute' : 'setAttribute']('disabled','disabled');
+    },
+    value: true
 });
 
 module.exports = FormElement;
